6dfe898f2a97b520119a05fc89aad5a70bb006f5
Rovanion Luckey <rovanion.luckey@gmail.com>
2011-04-10 10:25:46 +0200

    Halfway trough the change of using the actual components and not separate forms of the website for editing.

--- lib/content.php
+++ lib/content.php
@@ -1,8 +1,12 @@
+
 <?php
 require_once '../lib/settings.php';
 require_once '../lib/libJoy.php';
 require_once '../lib/libAuth.php';
 
+
+/*This file creates the content of each page. 
+  Concider merging it with ../lib/libJoy.php and ../pages/index.php*/
 
 if(isset($_GET["page"]))    //If there is a page variable, use it
   $table = mysql_real_escape_string($_GET["page"]);
@@ -77,31 +81,25 @@ function makeAdminInterface($post, $type){
   global $table;
   $id = $post["JStitle"];
 
-  if($type == 0 || $type == 1 || $type == 2 || $type == 4){ //If it's a text field
+  if($type == 0 || $type == 1 || $type == 2 || $type == 4){ //If it's a text field or open/closeall
+    echo '<div class="deleteEditContainer" id="'. $id. 'DeleteEditContainer">';
     if($type != 1) 
-      echo '<div class="button edit" id="'. $id. 'EditButton"><h3>Edit</h3></div>';
-    echo '<div class="deleteContainer"><div class="button delete" id="'. $id. 'DeleteButton">
-          <h3>Delete</h3></div></div>
-          <div class="yesNoContainer" id="'. $id. 'yesNoContainer"><h5>Är du säker?</h5>
-          <div class="button yes" id="'. $id. 'yesButton"><h3>Ja</h3></div>
-          <div class="button no" id="' . $id. 'noButton"><h3>Nej</h3></div></div>';
+      echo '<div class="button edit" id="'. $id. 'EditButton"><h3>Edit</h3></div>
+            <div id="'. $id. 'NicEditPanel"></div>';
+    echo '<div class="button delete" id="'. $id. 'DeleteButton"><h3>Delete</h3></div></div>
+          <div class="yesNoContainer" id="'. $id. 'YesNoContainer"><h5>Är du säker?</h5>
+              <div class="button yes" id="'. $id. 'YesButton"><h3>Ja</h3></div>
+              <div class="button no" id="' . $id. 'NoButton"><h3>Nej</h3></div></div>';
     if($type != 1){
-      echo '<div class="formContainer" id="'. $id. 'FormContainer">
-            <form id="'. $id. 'Form">';
-      if($type != 2)
-	echo     '<label for=title><h6>Titel</h6></label> <br />
-                  <input type="text" name="title" value="'. $post["title"]. '"><br />';
-      if($type != 4)
-	echo     '<label for=content><h6>Brödtext</h6></label>
-                  <textarea name=content>'. $post["content"]. '</textarea><br />';
-      echo        '<input type="hidden" name="index" value="'. $post["index"]. '" />
-                  <input type="hidden" name="table" value="'. $table. '" />
-                  <input type="hidden" name="submit" value="Spara">
-                  <input type="hidden" name="type" value="'. $type. '">
-    	      </form>
-              <div class="button submit" id="'. $id. 'SubmitButton"><h3>Spara</h3></div>
-              <div class="button cancelButton"><h3>Avbryt</h3></div>
-          </div>';
+      echo '<form id="'. $id. 'Form">
+                <input type="hidden" name="index" value="'. $post["index"]. '" />
+                <input type="hidden" name="table" value="'. $table. '" />
+                <input type="hidden" name="submit" value="Spara">
+                <input type="hidden" name="type" value="'. $type. '">
+    	    </form>
+            <div class="submitCancelContainer" id="'. $id. 'SubmitCancelContainer">
+                <div class="button submit" id="'. $id. 'SubmitButton"><h3>Spara</h3></div>
+                <div class="button cancelButton"><h3>Avbryt</h3></div></div>';
     }
   }
   else if($type == 3) echo 'FIX ME'; //FIXME
@@ -110,15 +108,16 @@ function makeAdminInterface($post, $type){
 
 function makeAddButton($id){
   global $table;
-  echo '<select name="type" class="type" id="'. $id. 'Type">
+  echo '<div class="newContainer">
+          <select name="type" class="type" id="'. $id. 'Type">
             <option value="0">Titel med gömd paragraf</option>
             <option value="1">Öppna och Stäng alla knappar</option>
             <option value="2">Paragraf</option>
             <option value="3">Bild med undertext</option>
             <option value="4">Huvudkategori</option>
-        </select>
-        <div class="button newButton" id="'. $id. 'NewButton"><h3>Lägg till
-        </h3></div>
+          </select>
+          <div class="button newButton" id="'. $id. 'NewButton"><h3>Lägg till
+        </h3></div></div>
         <div class="formContainer new" id="'. $id. 'FormContainer">
             <form id="'. $id. 'Form">
                 <div class="titleContainer">
@@ -134,7 +133,7 @@ function makeAddButton($id){
 	    </form>
         <div class="button submit" id="'. $id. 'SubmitButton"><h3>Skapa</h3></div>
         <div class="button cancelButton"><h3>Avbryt</h3></div>
-        </div><br /><br />';
+        </div>';
 }
 
 ?>
